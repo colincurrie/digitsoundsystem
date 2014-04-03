ccb49d16937af361864d14bca78d043ccbef7f5c
Colin Currie <ccurrie@marketsworld.com>
2014-04-03 21:39:24 +0100

    Added a rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Digit::Application.config.secret_key_base = 'a82c03f4c4f310fa741513d1be3e36b2a05bad534a03a3af2658d032c8312841658884a01aad52de20db1d7784b509c127f4e23ade132f7780dd4c42c069f23e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Digit::Application.config.session_store :cookie_store, key: '_digit_session'

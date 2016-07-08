807f42a0f73dd46a68caa52acf503e26aa7b466e
Colin Currie <ccurrie@marketsworld.com>
2016-07-08 11:35:35 +0100

    Adding a comment notification

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,9 +1,21 @@
 class AdminMailer < ApplicationMailer
 
-  default from: 'admininfo@digitsoundsystem.co.uk'
+  default from: 'digitsoundsystem@gmail.com'
 
   def new_user(user)
     @user = user
     mail(to: 'digitsoundsystem@gmail.com', subject: 'We got a new user')
+  end
+
+  def new_comment(comment)
+    @comment = comment
+    subject = comment.subject
+    if subject.is_a? Photo
+      @subject = "#{subject.class} #{subject.id}"
+    else
+      @subject = "the #{subject.class} '#{subject.title}'"
+    end
+    @link = url_for(subject)
+    mail(to: 'digitsoundsystem@gmail.com', subject: 'Somebody left a comment')
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,14 @@ class Comment < ActiveRecord::Base
   belongs_to :event
 
   validates_presence_of :content
+
+  after_create :comment_created
+
+  def comment_created
+      AdminMailer.new_comment(self).deliver_later # unless Rails.env == 'development'
+  end
+
+  def subject
+    story || photo || video || mixtape || event
+  end
 end

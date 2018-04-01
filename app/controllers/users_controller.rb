49760cd1b6b82e765fe4ebdd337619421ad98cbd
Colin Currie <ccurrie@marketsworld.com>
2018-04-01 15:40:16 +0100

    fixing user sign up for devise 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,12 @@ class UsersController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << [:admin, :name, :surname, :mailing_list]
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:admin, :name, :surname, :mailing_list])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update) << [:admin, :name, :surname, :mailing_list, :bio]
+    devise_parameter_sanitizer.permit(:account_update, keys: [:admin, :name, :surname, :mailing_list, :bio])
   end
 
 end
